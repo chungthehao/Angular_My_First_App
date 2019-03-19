c974b8281d13fe9c922310c750b9042188aef6f4
Chung Thế Hào <chungthehao@gmail.com>
2019-03-19 22:20:21 +0700

    Editing the first app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms'; // typescript cần để biết nó ở đâu
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    FormsModule // this is simply a feature understood by Angular since it's part of an Angular module,
+    // It tells Angular that we want to import some form features (để dùng thằng này: [(ngModel)]="name")
   ],
   providers: [],
   bootstrap: [AppComponent]
