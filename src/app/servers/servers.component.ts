34906281cc270bfa8b3a7e52467592c100b44a6e
Chung Thế Hào <chungthehao@gmail.com>
2019-03-21 23:14:51 +0700

    Working with Component Templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * - Trong @Component: BẮT BUỘC phải có "template" (template inline hoặc external file).
+ * - Còn selector (có cách khác load component vào thay cho selector) hay style có hay không cũng đc!
+ */
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
