bb0e009f04a1eb7afaf375270b1e5741e305a23d
Chung Thế Hào <chungthehao@gmail.com>
2019-03-21 22:49:48 +0700

    Understanding the Role of AppModule and Component Declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // typescript cần để biết nó ở đâu
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // Để typescript biết đường import từ đâu
 
 @NgModule({
+  // Tạo ra các files thôi chưa đủ, phải báo cho Angular biết nếu muốn xài
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent, // Để Angular biết mình muốn xài component này cho app module
   ],
   imports: [
     BrowserModule,
@@ -19,3 +22,10 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent] // Nơi liệt kê tất cả các component mà Angular cần biết khi nó phân tích file index.html --> This case: 'AppComponent'
 })
 export class AppModule { }
+
+
+/**
+ * Chức năng của modules
+ */
+// - Angular dùng các module để bundle các pieces của hệ thống mình (vd như các component) thành các packages
+// - Đưa cho Angular thông tin về tính năng nào hệ thống đang có
