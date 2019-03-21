b5ffe523f94654807c0e406f707db48223cece38
Chung Thế Hào <chungthehao@gmail.com>
2019-03-22 00:05:50 +0700

    Fully Understanding the Component Selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   // để ý <app-root></app-root> ở index.html, Angular dùng nó để nhận biết cái cần replace ở file index.html
   selector: 'app-root', 
+
   templateUrl: './app.component.html',
+  
   // styleUrls: ['./app.component.css'],
   styles: [`
     h3 {
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,12 +5,16 @@ import { Component, OnInit } from '@angular/core';
  * - Còn selector (có cách khác load component vào thay cho selector) hay style có hay không cũng đc!
  */
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // <app-servers></app-servers>
+  // selector: '[app-servers]', // <div app-servers></div>
+  // selector: '.app-servers', // <p class="app-servers"></p>
+
   // templateUrl: './servers.component.html',
   template: `
     <app-server></app-server>
     <app-server></app-server>
   `,
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
